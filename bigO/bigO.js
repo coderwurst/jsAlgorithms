c21b8d3626845f07aecf832d94f5da479435293d
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-06-06 22:36:53 +0200

    space complexity examples

--- bigO/bigO.js
+++ bigO/bigO.js
@@ -85,4 +85,50 @@ function subtotals(array) {
 }
 
 
+/** SPACE COMPLEXITY EXAMPLES **/
 
+/** O(1) **/
+function logUpTo(n) {
+    for (var i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+/** O(1) **/
+function logAtMost10(n) {
+    for (var i = 1; i <= Math.min(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+/** O(1) **/
+function logAtMost10(n) {
+    for (var i = 1; i <= Math.min(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+/** O(n) **/
+function onlyElementsAtEvenIndex(array) {
+    var newArray = Array(Math.ceil(array.length / 2));
+    for (var i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            newArray[i / 2] = array[i];
+        }
+    }
+    return newArray;
+}
+
+
+/** O(n) **/
+function subtotals(array) {
+    var subtotalArray = Array(array.length);
+    for (var i = 0; i < array.length; i++) {
+        var subtotal = 0;
+        for (var j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray[i] = subtotal;
+    }
+    return subtotalArray;
+}
