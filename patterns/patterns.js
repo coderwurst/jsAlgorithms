5d7fe7be48e7a2c2393938ec4391fbac8f2f19e7
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-06-24 21:01:56 +0200

    understanding the problem

---
+++ patterns/patterns.js
@@ -0,0 +1,19 @@
+/** Understanding the Problem **/
+
+// Write a function which takes 2 numbers and returns their sum
+
+// 1. Can I restate the problem in my own words?
+
+
+// 2. What are the inputs that go into the problem?
+
+
+// 3. What are the outputs that go into the problem?
+
+
+// 4. Can the output be determined from the inputs? Do I have enough info to save the problem?
+
+
+// 5. How should I label the important pieces of data that are part of the problem?
+
+
