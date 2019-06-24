c904a4c1565f830765ac4a76a6347ad82e9dd579
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-06-24 21:12:29 +0200

    exploring examples

--- patterns/patterns.js
+++ patterns/patterns.js
@@ -16,3 +16,23 @@
 
 // 5. How should I label the important pieces of data that are part of the problem?
     // num1, num2 , sum
+
+
+
+/** Exploring Examples **/
+// Write a function which takes in a string and returns counts of each character in the string
+
+// 1. Simple inputs
+charCount("aaaa");                          // { 4 }
+
+// 2. more complicated inputs
+charCount("some string with some chars")    // { 26 } do we need to count spaces?
+charCount("Hi hi hello")                    // h counted 3 times, i counted 2 times, or each 1 time
+
+// 3. empty inputs
+charCount("")                               // should return an error, or primpt to enter data?
+
+// 4. invalid inputs
+charCount(123)                              // should return an error, type checking?
+
+
