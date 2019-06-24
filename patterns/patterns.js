7e59634c7d0ce188f1bbe4627205bb5d5d98c575
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-06-24 21:06:03 +0200

    answers to understanding the problem

--- patterns/patterns.js
+++ patterns/patterns.js
@@ -3,17 +3,16 @@
 // Write a function which takes 2 numbers and returns their sum
 
 // 1. Can I restate the problem in my own words?
-
+    // implement addition
 
 // 2. What are the inputs that go into the problem?
-
+    // floats, integers, strings? Empty?
 
 // 3. What are the outputs that go into the problem?
-
+    // floats, integers, strings
 
 // 4. Can the output be determined from the inputs? Do I have enough info to save the problem?
-
+    // error handling, what happens if input is null
 
 // 5. How should I label the important pieces of data that are part of the problem?
-
-
+    // num1, num2 , sum
