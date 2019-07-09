49cab7277f3c8807ed0cac8651b46b1c0d972dd8
Andrew McMechan <coderwurst@gmail.com>
2019-07-09 21:38:56 +0200

    countdown and sum examples

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,4 +1,4 @@
-/** Call Stack Example - debug line 22 **/
+/** Call Stack Example - debug line 22 *
 function takeShower(){
     return "Showering!";
 }
@@ -19,4 +19,44 @@ function wakeUp() {
     console.log("ready to go to work!");
 }
 
-wakeUp();
+wakeUp();*/
+
+/** Countdown *
+function countdown(num){
+    if(num <= 0){       // Base Case
+        return;
+    }
+    console.log(num);
+    num--;              // update input
+    countdown(num);
+}
+
+countdown(10);*/
+
+
+/** Sum Range **/
+function sumRange(num) {
+    if(num ===1) return 1;
+    return num + sumRange(num - 1);
+}
+
+console.log(sumRange(3));
+// return 3 + sumRange(2)
+// return 2 + sumRange(1)
+// return 1
+// 1 + 2 + 3 = 6
+
+console.log(sumRange(4));
+// return 4 + sumRange(3)
+// return 3 + sumRange(2)
+// return 2 + sumRange(1)
+// return 1
+// 1 + 2 + 3 + 4 = 10
+
+console.log(sumRange(5));
+// return 5 + sumRange(4)
+// return 4 + sumRange(3)
+// return 3 + sumRange(2)
+// return 2 + sumRange(1)
+// return 1
+// 1 + 2 + 3 + 4 + 5 = 15
