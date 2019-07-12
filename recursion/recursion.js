a26aaba7c0634eef5af57815a02b5802bd9aeea8
Andrew McMechan <coderwurst@gmail.com>
2019-07-12 20:20:15 +0200

    factorial examplepure and helper recursion

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -34,7 +34,7 @@ function countdown(num){
 countdown(10);*/
 
 
-/** Sum Range **/
+/** Sum Range *
 function sumRange(num) {
     if(num ===1) return 1;
     return num + sumRange(num - 1);
@@ -59,4 +59,59 @@ console.log(sumRange(5));
 // return 3 + sumRange(2)
 // return 2 + sumRange(1)
 // return 1
-// 1 + 2 + 3 + 4 + 5 = 15
+// 1 + 2 + 3 + 4 + 5 = 15*/
+
+
+/** Factorial 4! = 4 * 3 * 2 * 1 *
+function factorial(num) {
+    let total = num;
+    if (num > 1) {          // base case = 1
+        return num * factorial(num - 1);
+    } else {
+        return total;
+    }
+}
+
+console.log(factorial(3));      // 3 * 2 * 1 = 6
+console.log(factorial(5));      // 5 * 4 * 3 * 2 * 1 = 120
+*/
+
+/** Helper method recursion *
+// psuedo code to show pattern - results stored in an array outside of recursive method
+function outer(input) {
+    var outerScopedVariable = []
+
+    function helper(helperInput){
+        if (helperInput === 0) {
+            return;
+        }
+
+        if(helperInput[0] % 2 !== 0) {
+            outerScopedVariable.push(helperInput[0])
+        }
+        
+        // modify outer scoped variable
+        helper(helperInput--);
+    }
+
+    helper(input)
+
+    return outerScopedVariable;
+}*/
+
+/** Pure Recursion *
+// self contained function, no nesting, no external data structures
+function collectOddValues(arr) {
+    let newArr = [];
+    if(arr.length === 0) {
+        return newArr;
+    }
+    if(arr[0] % 2 !== 0) {
+        newArr.push(arr[0]);
+    }
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+console.log(collectOddValues([1, 2, 3, 4, 5]));
+console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));*/
