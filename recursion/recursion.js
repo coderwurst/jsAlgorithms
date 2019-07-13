cbdc56e121cc1a58ea6459e0b118ca2c329bfdaa
Andrew McMechan <coderwurst@gmail.com>
2019-07-13 20:19:52 +0200

    fibonacci initial

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -161,7 +161,7 @@ console.log(productOfArray([1, 2, 3]));           // 6
 console.log(productOfArray([1, 2, 3, 10]));       // 60*/
 
 
-/** Recursive Range **/
+/** Recursive Range *
 // <p>Write a function called <strong>recursiveRange</strong> which accepts a 
 // number and adds up all the numbers from 0 to the number passed to the function&nbsp;</p>
 function recursiveRange(num){
@@ -171,5 +171,26 @@ function recursiveRange(num){
 
 console.log(recursiveRange(4));     // 10
 console.log(recursiveRange(6));     // 21
-console.log(recursiveRange(10));    // 55 
+console.log(recursiveRange(10));    // 55 */
+
+/** Fib **/
+// <p>Write a recursive function called <strong>fib</strong> which accepts a number 
+// and returns the <em>n</em>th number in the Fibonacci sequence. Recall that the 
+// Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which 
+// starts with 1 and 1, and where every number thereafter is equal to the sum of 
+// the previous two numbers.</p>
+function fib(num, arr = [1, 1]){
+    //console.log(arr);
+    if (num === 2) return arr[arr.length - 1];  // base case 2 as 1st 2 numbers added by default
+
+    const nextNumber = arr[arr.length - 1] + arr[arr.length - 2];
+    const newArr = [...arr, nextNumber];
+    
+    return fib(num - 1, newArr);
+}
+
+console.log(fib(4));        // 3
+console.log(fib(10));       // 55
+console.log(fib(28));       // 317811
+console.log(fib(35));       // 9227465
 
